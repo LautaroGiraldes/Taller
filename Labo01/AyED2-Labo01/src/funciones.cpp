cf0d730062b01a09fd4b2ec901781bc97305636b
Lautaro <lautigm@gmail.com>
2022-09-25 22:20:14 -0300

    labo01 sin hacer mas

--- Labo01/AyED2-Labo01/src/funciones.cpp
+++ Labo01/AyED2-Labo01/src/funciones.cpp
@@ -5,7 +5,15 @@ using namespace std;
 
 // Ejercicio 1
 vector<int> quitar_repetidos(vector<int> s) {
-    return vector<int>();
+    set<int> sin_repetidos;
+    for(int x:s){
+        sin_repetidos.insert(x);
+    }
+    vector<int> res;
+    for (int x:sin_repetidos) {
+        res.push_back(x);
+    }
+    return res;
 }
 
 // Ejercicio 2
@@ -20,22 +28,62 @@ bool mismos_elementos(vector<int> a, vector<int> b) {
 
 // Ejercicio 4
 bool mismos_elementos_v2(vector<int> a, vector<int> b) {
-    return true;
+    set<int> set_a;
+    for (int x:a){
+        set_a.insert(x);
+    }
+    set<int> set_b;
+    for (int x:b){
+        set_b.insert(x);
+    }
+    return set_a == set_b;
 }
 
 // Ejercicio 5
 map<int, int> contar_apariciones(vector<int> s) {
-    return map<int, int>();
+    map<int, int>res;
+    vector<int> sinRepetidos = quitar_repetidos(s);
+    int cantApariciones = 0;
+    for (int i = 0; i < sinRepetidos.size(); ++i) {
+        for (int j = 0; j < s.size(); ++j) {
+            if (sinRepetidos[i] == s[j]){
+                cantApariciones++;
+            }
+        }
+        res[sinRepetidos[i]] = cantApariciones;
+        cantApariciones = 0;
+    }
+    return res;
 }
 
 // Ejercicio 6
 vector<int> filtrar_repetidos(vector<int> s) {
-    return vector<int>();
+    map<int, int>rep = contar_apariciones(s);
+    vector<int> res;
+    for (int i=0; i < rep.size(); i++) {
+        if (rep[i] == 1){
+            res.push_back(i);
+        }
+    }
+    return res;
 }
 
 // Ejercicio 7
 set<int> interseccion(set<int> a, set<int> b) {
-    return set<int>();
+    set<int> res;
+    if (a.size() > b.size()){
+        for (int x:a ) {
+            if (b.count(x) == 1 ){
+                res.insert(x);
+            }
+        }
+    }else
+        for (int x:b ) {
+            if (a.count(x) == 1 ){
+                res.insert(x);
+            }
+        }
+    return res;
 }
 
 // Ejercicio 8
